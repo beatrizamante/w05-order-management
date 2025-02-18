43546a9971f884ac0535b53585abb49e3889efa2
Beatriz <beatrizamante@hotmail.com>
2025-02-18 17:56:56 -0300

    w05-OM-Task1: Fixes

--- database_setup.sql
+++ database_setup.sql
@@ -5,14 +5,13 @@ CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    active BOOLEAN NOT NULL
+    active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     customer_id INT NOT NULL,
     ordered_at TIMESTAMP NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
 
 CREATE TABLE products (
@@ -27,10 +26,36 @@ CREATE TABLE order_products (
     product_id INT NOT NULL,
     quantity INT NOT NULL DEFAULT 1,
     PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_customer
+FOREIGN KEY (customer_id) REFERENCES customers(id);
+
+ALTER TABLE order_products
+ADD CONSTRAINT fk_order
+FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE;
+
+ALTER TABLE order_products
+ADD CONSTRAINT fk_products
+FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE;
 
 SELECT setval('customers_id_seq', (SELECT MAX(id) FROM customers));
 SELECT setval('products_id_seq', (SELECT MAX(id) FROM products));
 SELECT setval('orders_id_seq', (SELECT MAX(id) FROM orders));
+
+COPY customers(id, first_name, last_name, active)
+FROM '/path/customers.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', QUOTE '"');
+
+COPY products(id, name, description, price_cents)
+FROM '/path/products.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', QUOTE '"');
+
+COPY orders(id, customer_id, ordered_at)
+FROM '/path/orders.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', QUOTE '"');
+
+COPY order_products(order_id, product_id, quantity)
+FROM '/path/customers.csv'
+WITH (FORMAT csv, HEADER true, DELIMITER ';', QUOTE '"');
