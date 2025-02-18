2cc5d5100f72daf1c1f084399a25cbd2f81325a0
Beatriz <beatrizamante@hotmail.com>
2025-02-18 17:12:20 -0300

    w05-OM-Task2: Fixes

--- most_expensive_product.sql
+++ most_expensive_product.sql
@@ -1,1 +1,6 @@
-SELECT id, name, TO_CHAR(price_cents / 100, 'FM999,999,999,999.00') as price FROM products WHERE price_cents = (SELECT MAX(price_cents) FROM products);
+SELECT 
+    id, name, TO_CHAR(price_cents / 100, 'FM999,999,999,999.00') as price 
+FROM 
+    products 
+WHERE 
+    price_cents = (SELECT MAX(price_cents) FROM products);
