64fe4723c69b65941872d1ef6b8c94cfc34755f2
Beatriz <beatrizamante@hotmail.com>
2025-02-18 18:02:08 -0300

    W05-OM-task2:
    Add all orders complete

---
+++ all_orders_from_last_year.sql
@@ -0,0 +1,11 @@
+SELECT 
+    orders.id,
+    orders.ordered_at,
+    customers.first_name || " " || customers.last_name as customer_name
+FROM 
+    orders
+JOIN 
+    customers ON customers.id = orders.customer_id
+WHERE  
+    orders.ordered_at BETWEEN DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 year'
+    AND DATE_TRUNC('year', CURRENT_DATE) - INTERVAL '1 day';
